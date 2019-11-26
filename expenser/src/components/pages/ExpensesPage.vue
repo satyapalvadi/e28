5b63af70abf8c0de707e75a93e6b682226750afb
Satya Palvadi <satyapalvadi@gmail.com>
2019-11-26 14:55:58 -0500

    Initial commit

---
+++ expenser/src/components/pages/ExpensesPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="blocks">
+    <expense-detail v-for="expense in expenses" :key="expense.id" :expense="expense"></expense-detail>
+  </div>
+</template>
+
+<script>
+import ExpenseDetail from "./../ExpenseDetail.vue";
+
+const axios = require("axios");
+
+export default {
+  name: "ExpensesPage",
+  components: { ExpenseDetail },
+  data: function() {
+    return {
+      expenses: []
+    };
+  },
+  props: [],
+  mounted: function() {
+    axios
+      .get(
+        "https://my-json-server.typicode.com/satyapalvadi/e28-expenser-api2/expenses/"
+      )
+      .then(response => {
+        this.expenses = response.data;
+        console.log(this.expenses);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ expenser/src/components/pages/FriendsPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="blocks">
+        <friend-detail v-for="friend in friends" :key="friend.id" :friend="friend"></friend-detail>
+    </div>
+</template>
+
+<script>
+
+import FriendDetail from "./../FriendDetail.vue";
+const axios = require("axios");
+
+export default {
+        name: "FriendsPage",
+
+    data: function(){
+        return {
+            friends: []
+         }
+    },
+
+    components: { FriendDetail },
+
+    props: [],
+
+    mounted: function() {
+    axios
+      .get(
+        "https://my-json-server.typicode.com/satyapalvadi/e28-expenser-api2/persons/"
+      )
+      .then(response => {
+        this.friends = response.data;
+        console.log(this.friends);
+      });
+  }
+
+    
+}
+</script>
+
+<style>
+
+</style>
